020b153f42b7179911721d0d9d8ec5e216f3fd83
jchery25 <jonathan.chery@yorkmail.cuny.edu>
2016-11-17 18:34:01 -0500

    Added handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -17,4 +17,12 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('/signup', (req, res) => {
+  res.render('SignUp');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
 module.exports = router;
